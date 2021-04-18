c148f8e5cd74c4c872dcd04dc50d59407aa1935b
Liutos <mat.liutos@gmail.com>
2021-04-18 21:03:34 +0800

    refactor: Use option `-group` to ensure that only one notification of a task pop up each time.
    Multiple notifications at the same time make no sense.

--- fledgling/cli/alerter.py
+++ fledgling/cli/alerter.py
@@ -1,5 +1,6 @@
 # -*- coding: utf8 -*-
 import subprocess
+from typing import List
 
 from fledgling.app.entity.task import Task
 from fledgling.app.use_case.event_loop import IAlerter
@@ -7,7 +8,7 @@ from fledgling.app.use_case.event_loop import IAlerter
 
 class Alerter(IAlerter):
     def alert(self, *, plan, task: Task):
-        args = []
+        args: List[str] = []
         args.append('alerter')
         args.append('-title')
         args.append(task.brief)
@@ -18,4 +19,5 @@ class Alerter(IAlerter):
             args.extend(['-message', '展示{}秒后自动关闭'.format(plan.duration)])
         else:
             args.extend(['-message', '需要手动关闭'])
+        args.extend(['-group', str(task.id)])
         return subprocess.Popen(args)
