e51849b98e94d7bf11f015b0f0b03e01befe27f9
Liutos <mat.liutos@gmail.com>
2021-12-05 20:15:32 +0800

    refactor: Use choice replacing arbitrary string input.

--- fledgling/cli/command/change_task.py
+++ fledgling/cli/command/change_task.py
@@ -10,12 +10,14 @@ from fledgling.cli.repository_factory import RepositoryFactory
 class Params(IParams):
     def __init__(self, task_id: int, *, brief: Optional[str] = None,
                  keywords: Optional[str] = None,
-                 status: Optional[int] = None):
+                 status: Optional[str] = None):
         self.brief = brief
         self.keywords = []
         if keywords is not None:
             self.keywords = keywords.split(',')
-        self.status = status
+        self.status = None
+        if status is not None:
+            self.status = int(status)
         self.task_id = task_id
 
     def get_brief(self) -> Tuple[bool, Optional[str]]:
@@ -34,10 +36,10 @@ class Params(IParams):
 @click.command()
 @click.option('--brief', help='任务简述', type=click.STRING)
 @click.option('--keywords', help='关键字', type=click.STRING)
-@click.option('--status', help='状态。2 表示已完成，3 表示已取消', type=click.INT)
+@click.option('--status', help='状态。2 表示已完成，3 表示已取消', type=click.Choice(['2', '3']))
 @click.option('--task-id', help='任务ID', type=click.INT)
 @click.pass_context
-def change_task(ctx: click.Context, *, brief, keywords, status, task_id):
+def change_task(ctx: click.Context, *, brief, keywords, status: str, task_id):
     """
     修改指定任务。
     """
