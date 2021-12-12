5ecea07fc878475019beb750ebf0671e5825cf61
Liutos <mat.liutos@gmail.com>
2021-12-12 14:42:22 +0800

    fix: `location_id` from CLI should be assigned to the same field.

--- fledgling/app/use_case/change_plan.py
+++ fledgling/app/use_case/change_plan.py
@@ -62,7 +62,7 @@ class ChangePlanUseCase:
             plan.duration = duration
         found, location_id = params.get_location_id()
         if found:
-            plan.keywords = location_id
+            plan.location_id = location_id
         found, repeat_interval = params.get_repeat_interval()
         if found:
             plan.task_id = repeat_interval
