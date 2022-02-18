013295c5b49f0673ea77fab1f571f0ea7bfe8f99
Liutos <mat.liutos@gmail.com>
2022-02-18 23:28:47 +0800

    feat: Add Elisp function for constructuring task and plan objects.

--- contrib/emacs/fledgling.el
+++ contrib/emacs/fledgling.el
@@ -1,4 +1,30 @@
 ;;; 私有的符号 BEGIN
+(defclass org-fledgling--plan ()
+  ((trigger-time
+    :documentation "任务被触发的时刻。"
+    :initarg :trigger-time)))
+
+(defclass org-fledgling--task ()
+  ((brief
+    :documentation "任务的简述"
+    :initarg :brief)
+   (plans
+    :documentation "该任务设定的计划。"
+    :initarg :plans)))
+
+(defun org-fledgling--cons-task-plan ()
+  "从当前光标所在的条目中构造出创建任务、计划所需要的数据。"
+  (let* ((brief (nth 4 (org-heading-components)))
+         (scheduled (org-entry-get nil "SCHEDULED"))
+         ;; TODO: 此处还需要先将 SCHEDULED 转换为 TRIGGER-TIME 的格式才行。
+         (plans nil))
+    (when scheduled
+      (setf plans (make-instance 'org-fledgling--plan
+                                 :trigger-time scheduled)))
+    (make-instance 'org-fledgling--task
+                   :brief brief
+                   :plans plans)))
+
 (defun org-fledgling--make-command (program args)
   "生成以参数 ARGS 调用程序 PROGRAM 的命令。"
   (assert (stringp program))
